8d22461d4e3b4ba484adec0670a1b92649290430
Deigon <deigon_prates@hotmail.com>
2017-04-08 13:50:29 -0300

    Corrigindo probles nos links

--- paginas/recupera.php
+++ paginas/recupera.php
@@ -12,16 +12,16 @@
         <meta name="description" content="A fully featured admin theme which can be used to build CRM, CMS, etc.">
         <meta name="author" content="Coderthemes">
 
-        <link rel="shortcut icon" href="../assets/images/favicon_1.ico">
+        <link rel="shortcut icon" href="/assets/images/favicon_1.ico">
 
         <title>SIEL - Recuperar senha</title>
 
-        <link href="../assets/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
-        <link href="../assets/css/core.css" rel="stylesheet" type="text/css" />
-        <link href="../assets/css/components.css" rel="stylesheet" type="text/css" />
-        <link href="../assets/css/icons.css" rel="stylesheet" type="text/css" />
-        <link href="../assets/css/pages.css" rel="stylesheet" type="text/css" />
-        <link href="../assets/css/responsive.css" rel="stylesheet" type="text/css" />
+        <link href="/assets/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+        <link href="/assets/css/core.css" rel="stylesheet" type="text/css" />
+        <link href="/assets/css/components.css" rel="stylesheet" type="text/css" />
+        <link href="/assets/css/icons.css" rel="stylesheet" type="text/css" />
+        <link href="/assets/css/pages.css" rel="stylesheet" type="text/css" />
+        <link href="/assets/css/responsive.css" rel="stylesheet" type="text/css" />
 
         <!-- HTML5 Shiv and Respond.js IE8 support of HTML5 elements and media queries -->
         <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -30,7 +30,7 @@
         <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
         <![endif]-->
 
-        <script src="../assets/js/modernizr.min.js"></script>
+        <script src="/assets/js/modernizr.min.js"></script>
 
     </head>
     <body>
@@ -74,20 +74,20 @@
         </script>
 
         <!-- jQuery  -->
-        <script src="../assets/js/jquery.min.js"></script>
-        <script src="../assets/js/bootstrap.min.js"></script>
-        <script src="../assets/js/detect.js"></script>
-        <script src="../assets/js/fastclick.js"></script>
-        <script src="../assets/js/jquery.slimscroll.js"></script>
-        <script src="../assets/js/jquery.blockUI.js"></script>
-        <script src="../assets/js/waves.js"></script>
-        <script src="../assets/js/wow.min.js"></script>
-        <script src="../assets/js/jquery.nicescroll.js"></script>
-        <script src="../assets/js/jquery.scrollTo.min.js"></script>
+        <script src="/assets/js/jquery.min.js"></script>
+        <script src="/assets/js/bootstrap.min.js"></script>
+        <script src="/assets/js/detect.js"></script>
+        <script src="/assets/js/fastclick.js"></script>
+        <script src="/assets/js/jquery.slimscroll.js"></script>
+        <script src="/assets/js/jquery.blockUI.js"></script>
+        <script src="/assets/js/waves.js"></script>
+        <script src="/assets/js/wow.min.js"></script>
+        <script src="/assets/js/jquery.nicescroll.js"></script>
+        <script src="/assets/js/jquery.scrollTo.min.js"></script>
 
 
-        <script src="../assets/js/jquery.core.js"></script>
-        <script src="../assets/js/jquery.app.js"></script>
+        <script src="/assets/js/jquery.core.js"></script>
+        <script src="/assets/js/jquery.app.js"></script>
 
     </body>
 </html>
