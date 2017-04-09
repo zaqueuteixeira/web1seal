421fbf042c601b9a2938a70de32ad8ac750d2d1d
deigon <deigon_prates@hotmail.com>
2017-04-09 17:27:37 -0300

    orientado a classe conexao

--- classes/conexao.class.php
+++ classes/conexao.class.php
@@ -1,12 +1,21 @@
 <?php
 
-$localhost = 'localhost';
-$user = 'root';
-$pass = '';
-$bd = 'seal';
+class Conexao {
 
-$mysqli = new mysqli($localhost, $user, $pass, $bd);
+    private $localhost = 'localhost';
+    private $user = 'root';
+    private $pass = '';
+    private $bd = 'seal';
 
-if(!$mysqli){
-    die('O siguintes erro foram encontrados: '. mysqli_connect_errno());
+    public function conectar() {
+
+        $mysqli = new mysqli($this->localhost, $this->user, $this->pass, $this->bd);
+
+        if (!$mysqli) {
+            die('O siguintes erro foram encontrados: ' . mysqli_connect_errno());
+        }
+        
+        return $mysqli;
+    }
+
 }
