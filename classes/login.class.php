dfd1c5c3f0301ae6ba74c5b5bc1851169d61ece8
deigon <deigon_prates@hotmail.com>
2017-04-09 01:14:34 -0300

    criando a classe resposavel por tudo sobre o login

---
+++ classes/login.class.php
@@ -0,0 +1,32 @@
+<?php
+require_once './cadastrar.class.php';;
+/*
+ * CLasse responsavel por tudo sobre o login
+ */
+
+class Login {
+
+    /**
+     * @author Deigon Prates <deigonprates@gmail.com>
+     * @return boolean true caso consiga cadastrar e false em caso de eventuais erros.
+     * @param string $data data da tentativa de login ex: 05/09/2017  
+     * @param string $user_id id do usuario a qual tentou logar  
+     */
+    public function registrarTentativaLogin($data, $user_id) {
+        $sql = "INSERT INTO tentativas_login values($data,$user_id)";
+        $teste = $mysqli->query($sql) or die(mysqli_errno($mysqli));
+        
+        return $teste;
+    }
+
+    /**
+     * @author Deigon Prates <deigonprates@gmail.com>
+     * @return o id do usuario
+     * @param string $data data da tentativa de login ex: 05/09/2017  
+     * @param string $user_id id do usuario a qual tentou logar  
+     */
+    public function retornarID($matricula) {
+        
+    }
+
+}
