2f2e026a6b3a970c99d9e8a2b6b18499e7880a91
deigon <deigon_prates@hotmail.com>
2017-04-09 20:43:27 -0300

    criando as funcoes de inserir e executar query

--- classes/conexao.class.php
+++ classes/conexao.class.php
@@ -8,7 +8,6 @@ class Conexao {
     private $bd = 'seal';
 
     public function BDConectar() {
-
         $conexao = mysqli_connect($this->localhost, $this->user, $this->pass, $this->bd) or die(mysqli_connect_error());
         mysqli_set_charset($conexao, 'utf8') or die(mysqli_error($conexao));
         
@@ -17,6 +16,25 @@ class Conexao {
     
     public function BDFechar($conexao) {
         @mysqli_close($conexao) or die(mysqli_error($conexao));
+    }
+    
+    public function DBGravar($tabela, array $dados){
+        $indices = implode(", ", $dados);
+        $valores = "'".implode("', '", $dados)."'";
+        
+        $sql = "INSERT INTO {$tabela}({$indices}) VALUES ({$valores})";
+        
+        return $this->BDExecutaQuery($sql);
+    }
+    
+    public function BDExecutaQuery($query){
+        $conexao = $this->BDConectar();
+        
+        $resultado = mysqli_query($conexao, $query) or die(mysqli_error($conexao));
+        
+        $this->BDFechar($conexao);
+        
+        return $resultado;
     }
 
 }
