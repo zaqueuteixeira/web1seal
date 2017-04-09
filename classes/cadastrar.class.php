3528fdae6bcfd7860e26a977c22c737ae36f6097
deigon <deigon_prates@hotmail.com>
2017-04-09 00:56:58 -0300

    fazendo o redirecionamento do usuario apos o cadastro

--- classes/cadastrar.class.php
+++ classes/cadastrar.class.php
@@ -12,7 +12,11 @@ $senha = md5($_POST['senha']);
 $repetaSenha = $_POST['repeta-senha'];
 $semestre = $_POST['semestre'];
 
-#matricula 	nome 	username 	email 	turma 	ano 	semestre 
 
 $sql = "INSERT INTO usuario(nome,matricula,username,email,turma,ano,senha,semestre) VALUES('$nome','$matricula','$username','$email','$turma','$ano','$senha', '$semestre')";
-$mysqli->query($sql) or die (mysqli_errno($mysqli));
+$teste = $mysqli->query($sql) or die(mysqli_errno($mysqli));
+
+session_start();
+$_SESSION['matricula'] = $_POST['matricula'];
+
+header("Location: /inicio");
--- header.php
+++ header.php
@@ -2,7 +2,7 @@
     session_start();
     
     if(!isset($_SESSION["matricula"])){
-        header("Location: ./login.php");
+        header("Location: /login");
         exit();
     }
 
