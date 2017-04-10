2fcae39d45fc0693a5b9849fe99b1a2c4cb56be7
deigon <deigon_prates@hotmail.com>
2017-04-09 22:00:16 -0300

    validando campus do cadastro

--- classes/cadastrar.class.php
+++ classes/cadastrar.class.php
@@ -9,27 +9,35 @@ $matricula = $_POST['matricula'];
 $email = $_POST['email'];
 $username = $_POST['username'];
 $turma = $_POST['turma'];
-$ano = $_POST['semestre'];
+$ano = $_POST['ano'];
 $senha = $_POST['senha'];
 $repetaSenha = $_POST['repeta-senha'];
 $semestre = $_POST['semestre'];
 
-$dados = [
-    'nome' => $nome,
-    'matricula' => $matricula,
-    'email' => $email,
-    'username' => $username,
-    'turma' => $turma,
-    'semestre' => $ano,
-    'senha' => md5($senha),
-    'semestre' => $semestre
-];
+if (!is_null($nome) && !is_null($matricula) && !is_null($email) && !is_null($username) && !is_null($turma) &&
+        !is_null($ano) && !is_null($senha) && !is_null($repetaSenha) && !is_null($semestre)) {
+    $dados = [
+        'nome' => $nome,
+        'matricula' => $matricula,
+        'email' => $email,
+        'username' => $username,
+        'turma' => $turma,
+        'ano' => $ano,
+        'senha' => md5($senha),
+        'semestre' => $semestre
+    ];
 
-$conexao->BDAbreConexao();
-    $sql = $conexao->DBGravar('usuario', $dados);
-$conexao->BDFecharConexao($conexao);
+    if ($senha === $repetaSenha) {
+        $con = $conexao->BDAbreConexao();
+        $sql = $conexao->DBGravar('usuario', $dados);
+        $conexao->BDFecharConexao($con);
 
-session_start();
-$_SESSION['matricula'] = $_POST['matricula'];
-
-header("Location: /inicio");
+        session_start();
+        $_SESSION['matricula'] = $_POST['matricula'];
+        header("Location: /inicio");
+    } else {
+        die("As senhas nao se coencidem");
+    }
+} else {
+    die("Todos os campos sao obrigatorios!");
+}
