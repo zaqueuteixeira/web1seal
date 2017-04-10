9cfe517f2f5ca87867a0e1236ab8eb1e483b3c00
deigon <deigon_prates@hotmail.com>
2017-04-10 19:08:27 -0300

    Comentando a classe conexao

--- classes/conexao.class.php
+++ classes/conexao.class.php
@@ -6,18 +6,36 @@ class Conexao {
     private $user = 'root';
     private $pass = '';
     private $bd = 'seal';
-
+    
+    /**
+     * @author Deigon Prates <deigonprates@gmail.com>
+     * @return obj Retorna um objeto contendo a conexao;
+     */
     public function BDAbreConexao() {
         $conexao = mysqli_connect($this->localhost, $this->user, $this->pass, $this->bd) or die(mysqli_connect_error());
         mysqli_set_charset($conexao, 'utf8') or die(mysqli_error($conexao));
 
         return $conexao;
     }
+    
+    /**
+     * @author Deigon Prates <deigonprates@gmail.com>
+     * @param obj $$conexao precisa do objeto da conexao para poder fecha-la. 
+     * @param obj $conexao
+     */
 
     public function BDFecharConexao($conexao) {
         mysqli_close($conexao) or die(mysqli_error($conexao));
     }
-
+    
+    /**
+     * @author Deigon Prates <deigonprates@gmail.com>
+     * 
+     * @param string $tabela nome da tabela 
+     * @param array $dados array associativo onde os indices sao os campos na tabela e os valores sao os valores que seram inseridos na tabela
+     * @return resultado da conexao 
+     * 
+     */
     public function DBGravar($tabela, array $dados) {
         $indices = implode(", ", array_keys($dados));
         $valores = "'" . implode("', '", $dados) . "'";
@@ -26,7 +44,14 @@ class Conexao {
 
         return $this->BDExecutaQuery($sql);
     }
-
+    
+    /**
+     * @author Deigon Prates <deigonprates@gmail.com>
+     * 
+     * @param string $query contendo a query do bd
+     * @return boolean 
+     */
+    
     public function BDExecutaQuery($query) {
         $conexao = $this->BDAbreConexao();
 
@@ -37,6 +62,15 @@ class Conexao {
         return $resultado;
     }
 
+    /**
+     * @author Deigon Prates <deigonprates@gmail.com>
+     * 
+     * @param string $tabela
+     * @param string $opcoes
+     * @param string $filtros
+     * @return array $dados com o resultado do select
+     */
+    
     public function BDSeleciona($tabela, $opcoes = '*', $filtros = null) {
         $filtros = ($filtros) ? " {$filtros}" : null;
 
@@ -53,6 +87,13 @@ class Conexao {
             return $dados;
         }
     }
+    
+    /*
+     * @author Deigon Prates <deigonprates@gmail.com>
+     *
+     * @param string $matricula do usuario a retornar a o id do aluno
+     * @return int $id    do usuario 
+     */
     
     public function BDRetornaID($matricula) {
         $matricula = (int) $matricula;
