8c7bc331ac4e8238f6097ae9ac9a1bd032bd3621
deigon <deigon_prates@hotmail.com>
2017-04-11 10:43:49 -0300

    impede o login caso nao exista tentativas

--- classes/autenticacao.class.php
+++ classes/autenticacao.class.php
@@ -13,17 +13,25 @@ $mysqli = $conexao->BDAbreConexao();
 
 $resultado = $conexao->BDSeleciona('usuario', '*', "WHERE(matricula like '$matricula' and senha = '$senha')");
 
+if ($login->checarTentativasLogin($matricula)) {
 
-if (!is_null($resultado[0]['id'])) {
-    session_start();
-    $_SESSION['matricula'] = $_POST['matricula'];
 
-    $login->excluirTentativasLogin($matricula);
+    if (!is_null($resultado[0]['id'])) {
+        session_start();
+        $_SESSION['matricula'] = $_POST['matricula'];
 
-    header("Location: /inicio");
-} else {
-    $login->registrarTentativaLogin($matricula);
-    header("Location: /login");
-}
+        $login->excluirTentativasLogin($matricula);
+
+        header("Location: /inicio");
+    } else {
+        $conexao->BDFecharConexao($mysqli);
+        $login->registrarTentativaLogin($matricula);
+        header("Location: /login");
+    }
+
+    }else{
+        die("usuario bloqueado");
+    }
+
 
 $conexao->BDFecharConexao($mysqli);
