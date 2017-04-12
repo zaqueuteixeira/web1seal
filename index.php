d4d643f42e7ac945a988d834a8612f9558c1b1ba
deigon <deigon_prates@hotmail.com>
2017-04-11 23:21:31 -0300

    adicionando subdiretorios ao htaccess

--- index.php
+++ index.php
@@ -1,5 +1,8 @@
 <?php
 
+define("PASTA", "./paginas/");
+
+
 $REQUEST_URI = filter_input(INPUT_SERVER, 'REQUEST_URI');
 $INITE = strpos($REQUEST_URI, '?');
 
@@ -11,8 +14,15 @@ $REQUEST_URI_PASTA = substr($REQUEST_URI, 1);
 $URL = explode('/', $REQUEST_URI_PASTA);
 $URL[0] = ($URL[0] != '' ? $URL[0] : 'login');
 
-if (file_exists('./paginas/' . $URL[0] . '.php')):
-    require('./paginas/' . $URL[0] . '.php');
+if (file_exists(PASTA . $URL[0] . '.php')):
+    require(PASTA . $URL[0] . '.php');
+
+elseif (is_dir(PASTA . $URL[0])):
+    if (isset($URL[1]) && file_exists(PASTA . $URL[0] . '/' . $URL[1] . '.php')):
+        require(PASTA . $URL[0] . '/' . $URL[1] . '.php');
+    else:
+        require(PASTA . '404.php');
+    endif;
 else:
-    require('./paginas/404.html');
+    require(PASTA . '404.html');
 endif;
