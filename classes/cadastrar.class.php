2ed9408a2292b1b9dc1082dd7705d77c97a4ac9b
deigon <deigon_prates@hotmail.com>
2017-04-12 15:11:16 -0300

    fazendo a classe cadastrar orientada ao objeto

--- classes/cadastrar.class.php
+++ classes/cadastrar.class.php
@@ -1,53 +1,20 @@
 <?php
 
-require_once './conexao.class.php';
+require_once './classes/conexao.class.php';
 
-$conexao = new Conexao();
+class Cadastrar extends Conexao {
 
-$nome = $_POST['nome'];
-$matricula = $_POST['matricula'];
-$email = $_POST['email'];
-$username = $_POST['username'];
-$turma = $_POST['turma'];
-$ano = $_POST['ano'];
-$senha = $_POST['senha'];
-$repetaSenha = $_POST['repeta-senha'];
-$semestre = $_POST['semestre'];
+    public function cadastrarAluno($dados) {
 
-if (!is_null($nome) && !is_null($matricula) && !is_null($email) && !is_null($username) && !is_null($turma) &&
-        !is_null($ano) && !is_null($senha) && !is_null($repetaSenha) && !is_null($semestre)) {
-    $dados = [
-        'nome' => $nome,
-        'matricula' => $matricula,
-        'email' => $email,
-        'username' => $username,
-        'turma' => $turma,
-        'ano' => $ano,
-        'senha' => md5($senha),
-        'semestre' => $semestre
-    ];
-
-    if ($conexao->BDSeleciona('usuario', 'matricula', "WHERE(matricula like'{$matricula}')")) {
-        die("Matricula ja encontra-se em uso no nosso sistema!");
+        var_dump($dados);
     }
-    if ($conexao->BDSeleciona('usuario', 'email', "WHERE(email like'{$email}')")) {
-        die("Email ja encontra-se em uso no nosso sistema!");
+    
+    public function cadastrarAtividade(array $dados) {
+        
     }
-    if ($senha === $repetaSenha) {
+    
+    public function cadastrarAvaliacao(array $dados) {
         
-        $con = $conexao->BDAbreConexao();
-        
-        $sql = $conexao->DBGravar('usuario', $dados);
-        
-        $conexao->BDFecharConexao($con);
-        
-        session_start();
-        $_SESSION['matricula'] = $_POST['matricula'];
-        
-        header("Location: /inicio");
-    } else {
-        die("As senhas nao se coencidem");
     }
-} else {
-    die("Todos os campos sao obrigatorios!");
+
 }
