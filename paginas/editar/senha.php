732fe05ddcfcfd3a02d5324da896b4216ce7a10f
deigon <deigon_prates@hotmail.com>
2017-04-12 20:27:27 -0300

    finalizando o editar senha do usuario

--- paginas/editar/senha.php
+++ paginas/editar/senha.php
@@ -1,59 +1,62 @@
 <?php
 require_once './header.php';
 ?>
-<!-- Page-Title -->
-<div class="row">
-    <div class="col-sm-12">
-        <ol class="breadcrumb">
-            <li>
-                <a href="/inicio">Inicio</a>
-            </li>
-
-            <li class="active">
-                editar
-            </li>
-            <li>
-                <a href="/editar/senha">Senha</a>
-            </li>
-        </ol>
-    </div>
-</div>
 <div class="row">
     <div class="col-sm-12">
         <div class="card-box">
+            <!-- Page-Title -->
             <div class="row">
-                <div class="col-md-7">
-                    <form class="form-horizontal" role="form">                                    
-                        <div class="form-group">
-                            <label class="col-md-3 control-label">Senha antiga</label>
-                            <div class="col-md-9">
-                                <input type="password" class="form-control" value="password">
-                            </div>
-                        </div>
-                        <div class="form-group">
-                            <label class="col-md-3 control-label">Nova Senha</label>
-                            <div class="col-md-9">
-                                <input type="password" class="form-control" value="password">
-                            </div>
-                        </div>
-                        <div class="form-group">
-                            <label class="col-md-3 control-label">Repita a nova Senha</label>
-                            <div class="col-md-9">
-                                <input type="password" class="form-control" value="password">
-                            </div>
-                        </div>
-                        <div class="form-group m-b-0">
-                            <div class="col-sm-offset-3 col-sm-9">
-                                <button type="submit" class="btn btn-info waves-effect waves-light">Salvar</button>
-                            </div>
-                        </div>
-
-                    </form>
+                <div class="col-sm-12">
+                    <ol class="breadcrumb">
+                        <li>
+                            <a href="/inicio">Inicio</a>
+                        </li>
+                        <li class="active">
+                            editar
+                        </li>
+                        <li>
+                            <a href="/editar/senha">Senha</a>
+                        </li>
+                    </ol>
                 </div>
             </div>
+            <div class="row">
+
+                <center><h4 class="page-title">Edite a senha do Usuário</h4></center>
+                <br>
+                <br>
+
+                <form class="form-horizontal" role="form">                                    
+                    <div class="form-group">
+                        <label class="col-md-4 control-label">Matricula</label>
+                        <div class="col-md-4">
+                            <input type="password" class="form-control" value="">
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label class="col-md-4 control-label">Nova Senha</label>
+                        <div class="col-md-4">
+                            <input type="password" class="form-control" value="">
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label class="col-md-4 control-label">Repita a Senha</label>
+                        <div class="col-md-4">
+                            <input type="password" class="form-control" value="">
+                        </div>
+                        
+                    </div>
+                        <div class="form-group m-b-0">
+                            <div class="col-sm-offset-5 col-sm-2">
+                                <button type="submit" class="btn btn-info waves-effect waves-light">Atualizar</button>
+                            </div>
+                        </div>
+                </form>
+            </div>
         </div>
+    </div>
 
 
-        <?php
-        require_once './footer.php';
-        ?>
+    <?php
+    require_once './footer.php';
+    ?>
