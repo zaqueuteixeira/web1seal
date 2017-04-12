fdcdbc64159ae747642fec0651975a65606c0cf4
deigon <deigon_prates@hotmail.com>
2017-04-12 17:45:53 -0300

    validando os campos do cadastro de usuario

--- classes/cadastrar.class.php
+++ classes/cadastrar.class.php
@@ -1,19 +1,26 @@
 <?php
 
+require_once './classes/validarCampos.php';
 require_once './classes/conexao.class.php';
 
 class Cadastrar extends Conexao {
 
     public function cadastrarAluno($dados) {
-        unset($dados['repeta-senha']);
         
-        $this->DBGravar('usuario', $dados);
+        $validar = new ValidarCampos();
+        $objValidar = $validar->ValidarCadastroUsuario($dados);
+        
+        if($objValidar->status === TRUE) {
+            $this->DBGravar('usuario', $dados);
+        }else{
+            print_r($objValidar->erro);
+        }
     }
-    
+
     public function cadastrarAtividade(array $dados) {
         
     }
-    
+
     public function cadastrarAvaliacao(array $dados) {
         
     }
