472c183959ae91692c2404a84c6a60081128855d
deigon <deigon_prates@hotmail.com>
2017-04-12 20:31:38 -0300

    corrigindo bugs no fechamento das divs

--- paginas/editar/perfil.php
+++ paginas/editar/perfil.php
@@ -21,37 +21,37 @@ require_once './header.php';
                 </div>
             </div>
             <div class="row">
-                
-                    <center><h4 class="page-title">Edite seu perfil</h4></center>
-                    <br>
-                    <br>
-                    
-                    <form class="form-horizontal" role="form">                                    
-                        <div class="form-group">
-                            <label class="col-md-2 control-label">Nome completo:</label>
-                            <div class="col-md-5">
-                                <input type="text" class="form-control" value="">
-                            </div>
-                            <label class="col-md-1 control-label">Username</label>
-                            <div class="col-md-3">
-                                <input type="text" class="form-control" value="">
-                            </div>
+
+                <center><h4 class="page-title">Edite seu perfil</h4></center>
+                <br>
+                <br>
+
+                <form class="form-horizontal" role="form">                                    
+                    <div class="form-group">
+                        <label class="col-md-2 control-label">Nome completo:</label>
+                        <div class="col-md-5">
+                            <input type="text" class="form-control" value="">
                         </div>
-                        <div class="form-group">
-                            <label class="col-md-2 control-label" for="example-email">Email</label>
-                            <div class="col-md-5">
-                                <input type="email" id="example-email" name="example-email" class="form-control" placeholder="Email">
-                            </div>
-                            <label class="col-md-1 control-label">Matricula</label>
-                            <div class="col-md-3">
-                                <input disabled="" type="text" class="form-control" value="">
-                            </div>
+                        <label class="col-md-1 control-label">Username</label>
+                        <div class="col-md-3">
+                            <input type="text" class="form-control" value="">
                         </div>
-                        <div class="form-group">
-                            <label class="col-md-2 control-label">Turma</label>
-                            <div class="col-md-2">
-                                <input disabled="" type="text" class="form-control" value="">
-                            </div>
+                    </div>
+                    <div class="form-group">
+                        <label class="col-md-2 control-label" for="example-email">Email</label>
+                        <div class="col-md-5">
+                            <input type="email" id="example-email" name="example-email" class="form-control" placeholder="Email">
+                        </div>
+                        <label class="col-md-1 control-label">Matricula</label>
+                        <div class="col-md-3">
+                            <input disabled="" type="text" class="form-control" value="">
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label class="col-md-2 control-label">Turma</label>
+                        <div class="col-md-2">
+                            <input disabled="" type="text" class="form-control" value="">
+                        </div>
                         <div class="form-group">
                             <label class="col-md-1 control-label">Ano</label>
                             <div class="col-md-2">
@@ -62,37 +62,38 @@ require_once './header.php';
                                 <input disabled="" type="text" class="form-control" value="">
                             </div>
                         </div>
-                              <div class="form-group">
-                            <label class="col-md-2 control-label">Senha antiga</label>
-                            <div class="col-md-5">
-                                <input type="password" class="form-control" value="">
-                            </div>
-                        </div>
-                        <div class="form-group">
-                            <label class="col-md-2 control-label">Nova Senha</label>
-                            <div class="col-md-3">
-                                <input type="password" class="form-control" value="">
-                            </div>
-                            <label class="col-md-3 control-label">Repita a Senha</label>
-                            <div class="col-md-3">
-                                <input type="password" class="form-control" value="">
-                            </div>
-                        </div>
-                            
-                        <div class="form-group m-b-0">
-                            <div class="col-sm-offset-5 col-sm-9">
-                                <button type="submit" class="btn btn-info waves-effect waves-light">Atualizar</button>
-                            </div>
-                        </div>
 
-                    </form>
-                </div>
-            
-        </div>   
+                    </div>
+                    <div class="form-group">
+                        <label class="col-md-2 control-label">Senha antiga</label>
+                        <div class="col-md-5">
+                            <input type="password" class="form-control" value="">
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label class="col-md-2 control-label">Nova Senha</label>
+                        <div class="col-md-3">
+                            <input type="password" class="form-control" value="">
+                        </div>
+                        <label class="col-md-3 control-label">Repita a Senha</label>
+                        <div class="col-md-3">
+                            <input type="password" class="form-control" value="">
+                        </div>
+                    </div>
+
+                    <div class="form-group m-b-0">
+                        <div class="col-sm-offset-5 col-sm-9">
+                            <button type="submit" class="btn btn-info waves-effect waves-light">Atualizar</button>
+                        </div>
+                    </div>
+
+                </form>
+
+            </div>   
+        </div>
     </div>
-</div>
 
 
-<?php
-require_once './footer.php';
-?>
+    <?php
+    require_once './footer.php';
+    ?>
