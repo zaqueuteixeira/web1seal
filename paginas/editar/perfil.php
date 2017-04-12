6dd9f2d39dfb26f19da2db8419cc7dc7be78510e
deigon <deigon_prates@hotmail.com>
2017-04-12 20:29:11 -0300

    renomeando o botao de salvar => atualizar

--- paginas/editar/perfil.php
+++ paginas/editar/perfil.php
@@ -81,7 +81,7 @@ require_once './header.php';
                             
                         <div class="form-group m-b-0">
                             <div class="col-sm-offset-5 col-sm-9">
-                                <button type="submit" class="btn btn-info waves-effect waves-light">Salvar</button>
+                                <button type="submit" class="btn btn-info waves-effect waves-light">Atualizar</button>
                             </div>
                         </div>
 
