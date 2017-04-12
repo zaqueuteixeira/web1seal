decf92ff4050e076681db3732a745f2e8ec2a98c
deigon <deigon_prates@hotmail.com>
2017-04-12 00:25:32 -0300

    adicionando formulario ao atualizar senha

--- paginas/editar/senha.php
+++ paginas/editar/senha.php
@@ -1,15 +1,59 @@
 <?php
 require_once './header.php';
 ?>
+<!-- Page-Title -->
+<div class="row">
+    <div class="col-sm-12">
+        <ol class="breadcrumb">
+            <li>
+                <a href="/inicio">Inicio</a>
+            </li>
+
+            <li class="active">
+                editar
+            </li>
+            <li>
+                <a href="/editar/senha">Senha</a>
+            </li>
+        </ol>
+    </div>
+</div>
 <div class="row">
     <div class="col-sm-12">
         <div class="card-box">
-            conteudo vem aqui
+            <div class="row">
+                <div class="col-md-7">
+                    <form class="form-horizontal" role="form">                                    
+                        <div class="form-group">
+                            <label class="col-md-3 control-label">Senha antiga</label>
+                            <div class="col-md-9">
+                                <input type="password" class="form-control" value="password">
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label class="col-md-3 control-label">Nova Senha</label>
+                            <div class="col-md-9">
+                                <input type="password" class="form-control" value="password">
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label class="col-md-3 control-label">Repita a nova Senha</label>
+                            <div class="col-md-9">
+                                <input type="password" class="form-control" value="password">
+                            </div>
+                        </div>
+                        <div class="form-group m-b-0">
+                            <div class="col-sm-offset-3 col-sm-9">
+                                <button type="submit" class="btn btn-info waves-effect waves-light">Salvar</button>
+                            </div>
+                        </div>
+
+                    </form>
+                </div>
+            </div>
         </div>
-    </div>
-</div>
 
 
-<?php
-require_once './footer.php';
-?>
+        <?php
+        require_once './footer.php';
+        ?>
