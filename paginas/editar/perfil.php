f8829419f5fafd06a3af9e77fdcc3919e53ebca6
deigon <deigon_prates@hotmail.com>
2017-04-13 12:46:42 -0300

    populando dados do editar perfil

--- paginas/editar/perfil.php
+++ paginas/editar/perfil.php
@@ -1,5 +1,30 @@
 <?php
 require_once './header.php';
+require_once './classes/conexao.class.php';
+
+$conexao = new Conexao();
+
+$con = $conexao->BDAbreConexao();
+
+$nome = $conexao->BDSeleciona('usuario', 'nome', "WHERE(matricula like '{$_SESSION['matricula']}')");
+$nome = $nome[0]['nome'];
+
+$email = $conexao->BDSeleciona('usuario', 'email', "WHERE(matricula like '{$_SESSION['matricula']}')");
+$email = $email[0]['email'];
+
+$username = $conexao->BDSeleciona('usuario', 'username', "WHERE(matricula like '{$_SESSION['matricula']}')");
+$username = $username[0]['username'];
+
+$turma = $conexao->BDSeleciona('usuario', 'turma', "WHERE(matricula like '{$_SESSION['matricula']}')");
+$turma = $turma[0]['turma'];
+
+$semestre = $conexao->BDSeleciona('usuario', 'semestre', "WHERE(matricula like '{$_SESSION['matricula']}')");
+$semestre = $semestre[0]['semestre'];
+
+$ano = $conexao->BDSeleciona('usuario', 'ano', "WHERE(matricula like '{$_SESSION['matricula']}')");
+$ano = $ano[0]['ano'];
+
+$conexao->BDFecharConexao($con);
 ?>
 <div class="row">
     <div class="col-sm-12">
@@ -30,68 +55,66 @@ require_once './header.php';
                     <div class="form-group">
                         <label class="col-md-2 control-label">Nome completo:</label>
                         <div class="col-md-5">
-                            <input type="text" class="form-control" name="nome" value="">
+                            <input type="text" class="form-control" name="nome" value="<?php echo $nome; ?>">
                         </div>
                         <label class="col-md-1 control-label">Username</label>
                         <div class="col-md-3">
-                            <input type="text" class="form-control" name="username" value="">
+                            <input type="text" class="form-control" name="username" value="<?php echo $username; ?>">
                         </div>
                     </div>
                     <div class="form-group">
                         <label class="col-md-2 control-label" for="email">Email</label>
                         <div class="col-md-5">
-                            <input type="email" id="example-email" name="email" class="form-control" placeholder="Email">
+                            <input type="email" id="example-email" name="email" class="form-control" value="<?php echo $email; ?>" placeholder="Email">
                         </div>
                         <label class="col-md-1 control-label">Matricula</label>
                         <div class="col-md-3">
-                            <input disabled="" type="text" class="form-control" name="matricula" value="">
+                            <input disabled="" type="text" class="form-control" name="matricula" value="<?php echo $_SESSION['matricula'] ?>">
                         </div>
                     </div>
                     <div class="form-group">
                         <label class="col-md-2 control-label">Turma</label>
                         <div class="col-md-2">
-                            <input disabled="" type="text" class="form-control" name="turma" value="">
+                            <input type="text" class="form-control" name="turma" value="<?php echo $turma; ?>">
                         </div>
-                        <div class="form-group">
                             <label class="col-md-1 control-label">Ano</label>
                             <div class="col-md-2">
-                                <input disabled="" type="text" class="form-control" name="ano" value="">
+                                <input  type="text" class="form-control" name="ano" value="<?php echo $ano; ?>">
                             </div>
                             <label class="col-md-1 control-label">Semestre</label>
-                            <div class="col-md-2">
-                                <input disabled="" type="text" class="form-control" name="semestre" value="">
+                            <div class="col-md-3">
+                                <input  type="text" class="form-control" name="semestre" value="<?php echo $semestre; ?>">
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label class="col-md-2 control-label">Senha antiga</label>
+                            <div class="col-md-5">
+                                <input type="password" class="form-control" name="senha-antiga" value="<?php ?>" placeholder="**********">
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label class="col-md-2 control-label">Nova Senha</label>
+                            <div class="col-md-3">
+                                <input type="password" class="form-control" name="senha-nova" value="<?php ?>"placeholder="**********">
+                            </div>
+                            <label class="col-md-3 control-label">Repita a Senha</label>
+                            <div class="col-md-3">
+                                <input type="password" class="form-control" name="repeta-senha" value="<?php ?>"placeholder="**********">
                             </div>
                         </div>
 
-                    </div>
-                    <div class="form-group">
-                        <label class="col-md-2 control-label">Senha antiga</label>
-                        <div class="col-md-5">
-                            <input type="password" class="form-control" name="senha-antiga" value="">
+                        <div class="form-group m-b-0">
+                            <div class="col-sm-offset-5 col-sm-9">
+                                <button type="submit" class="btn btn-info waves-effect waves-light">Atualizar</button>
+                            </div>
                         </div>
-                    </div>
-                    <div class="form-group">
-                        <label class="col-md-2 control-label">Nova Senha</label>
-                        <div class="col-md-3">
-                            <input type="password" class="form-control" name="senha-nova" value="">
-                        </div>
-                        <label class="col-md-3 control-label">Repita a Senha</label>
-                        <div class="col-md-3">
-                            <input type="password" class="form-control" name="repeta-senha" value="">
-                        </div>
-                    </div>
-
-                    <div class="form-group m-b-0">
-                        <div class="col-sm-offset-5 col-sm-9">
-                            <button type="submit" class="btn btn-info waves-effect waves-light">Atualizar</button>
-                        </div>
-                    </div>
                 </form>
             </div>   
         </div>
     </div>
+</div>
 
 
-    <?php
-    require_once './footer.php';
-    ?>
+<?php
+require_once './footer.php';
+?>
