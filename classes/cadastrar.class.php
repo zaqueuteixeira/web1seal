c1c015e474f56c7bb7b845d7bf5264e35715da0e
deigon <deigon_prates@hotmail.com>
2017-04-15 00:26:29 -0300

    implementando a funcao de  cadastro de turma

--- classes/cadastrar.class.php
+++ classes/cadastrar.class.php
@@ -6,13 +6,13 @@ require_once './classes/conexao.class.php';
 class Cadastrar extends Conexao {
 
     public function cadastrarAluno($dados) {
-        
+
         $validar = new ValidarCampos();
         $objValidar = $validar->ValidarCadastroUsuario($dados);
 
         if ($objValidar->status === TRUE) {
             $this->DBGravar('usuario', $objValidar->dados);
-            
+
             session_start();
             $_SESSION['matricula'] = $_POST['matricula'];
 
@@ -23,13 +23,13 @@ class Cadastrar extends Conexao {
     }
 
     public function cadastrarAtividade($dados) {
-        
+
         $validar = new ValidarCampos();
         $objValidar = $validar->validarCadastroAtividade($dados);
 
         if ($objValidar->status === TRUE) {
             $this->DBGravar('atividade', $objValidar->dados);
-            
+
             header("Location: /inicio");
         } else {
             print_r($objValidar->erro);
@@ -37,13 +37,26 @@ class Cadastrar extends Conexao {
     }
 
     public function cadastrarAvaliacao($dados) {
-        
+
         $validar = new ValidarCampos();
         $objValidar = $validar->ValidarCadastroUsuario($dados);
 
         if ($objValidar->status === TRUE) {
             $this->DBGravar('avaliacao', $objValidar->dados);
-            
+
+            header("Location: /inicio");
+        } else {
+            print_r($objValidar->erro);
+        }
+    }
+
+    public function cadastrarTurma($dados) {
+        $validar = new ValidarCampos();
+        $objValidar = $validar->validarCadastroTurma($dados);
+        
+        if ($objValidar->status) {
+            $this->DBGravar('turma', $objValidar->dados);
+
             header("Location: /inicio");
         } else {
             print_r($objValidar->erro);
