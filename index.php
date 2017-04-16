22573de6cdb739f2d03cdf9c3cbdac3f597e5153
deigon <deigon_prates@hotmail.com>
2017-04-16 16:40:42 -0300

    corrigindo bug de salvar a mesma coisa 2 vezes no banco

--- index.php
+++ index.php
@@ -45,25 +45,16 @@ if ((isset($URL[0])) && (isset($URL[1])) && ($URL[0] . '/' . $URL[1] == 'cadastr
     $cadastrar = new Cadastrar();
     $cadastrar->cadastrarTurma($_POST);
 endif;
-
 if ((isset($URL[0])) && ($URL[0] == 'autenticacao')):
     $autenticacao = new Autenticacao();
     $autenticacao->autenticarUsuario($_POST);
 endif;
-
-
 if (file_exists(PASTA . $URL[0] . '.php')):
     require(PASTA . $URL[0] . '.php');
 
 elseif ($URL[0] . '/' . $URL[1] == 'cadastrando/usuario'):
     $cadastrar = new Cadastrar();
     $cadastrar->cadastrarAluno($_POST);
-elseif ($URL[0] . '/' . $URL[1] == 'cadastrando/atividade'):
-    $cadastrar = new Cadastrar();
-    $cadastrar->cadastrarAtividade($_POST);
-elseif ($URL[0] . '/' . $URL[1] == 'cadastrando/avaliacao'):
-    $cadastrar = new Cadastrar();
-    $cadastrar->cadastrarAvaliacao($_POST);
 elseif (is_dir(PASTA . $URL[0])):
     if (isset($URL[1]) && file_exists(PASTA . $URL[0] . '/' . $URL[1] . '.php')):
         require(PASTA . $URL[0] . '/' . $URL[1] . '.php');
