ab61853a5ab336671a14f5c2bb16ef9f27263167
Deigon <deigon_prates@hotmail.com>
2017-05-06 02:24:27 -0300

    refactorando o codigo

--- paginas/inicio.php
+++ paginas/inicio.php
@@ -9,24 +9,23 @@ require_once "$header";
 <div class="row">
     <div class="col-sm-12">
         <div class="card-box">
-            conteudo vem aqui
+            oi eu sou o goku!
         </div>
     </div>
 </div>
 <!--Start of Tawk.to Script-->
 <script type="text/javascript">
-var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
-(function(){
-var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-s1.async=true;
-s1.src='https://embed.tawk.to/58f74c61f7bbaa72709c710f/default';
-s1.charset='UTF-8';
-s1.setAttribute('crossorigin','*');
-s0.parentNode.insertBefore(s1,s0);
-})();
+    var Tawk_API = Tawk_API || {}, Tawk_LoadStart = new Date();
+    (function () {
+        var s1 = document.createElement("script"), s0 = document.getElementsByTagName("script")[0];
+        s1.async = true;
+        s1.src = 'https://embed.tawk.to/58f74c61f7bbaa72709c710f/default';
+        s1.charset = 'UTF-8';
+        s1.setAttribute('crossorigin', '*');
+        s0.parentNode.insertBefore(s1, s0);
+    })();
 </script>
 <!--End of Tawk.to Script-->
-
 <?php
 require_once './footer.php';
 ?>
